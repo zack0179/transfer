4bb9d29bf6a19a9cc22856f0507637bd911392b6
dpitonyak <dap67@psu.edu>
2018-03-01 15:57:41 -0500

    AN stuff

---
+++ database/pp_AN/README.md
@@ -0,0 +1,17 @@
+# pp AN DATABASE
+
+| index | ref          | experiment | h     | obs              | points | dependence |
+| :--:  | :--:         | :--:       | :--:  | :--:             | :--:   | :--:       |
+| 1000  | [link][1000] | BRAHMS     |  pim  | AN-theta2.3      | 3      | xF,pT      |
+| 1001  | [link][1000] | BRAHMS     |  pim  | AN-theta4.0      | 6      | xF,pT      |
+| 1002  | [link][1000] | BRAHMS     |  pip  | AN-theta2.3      | 3      | xF,pT      |
+| 1003  | [link][1000] | BRAHMS     |  pip  | AN-theta4.0      | 6      | xF,pT      |
+| 2000  | [link][2000] | STAR       |  pi0  | AN-2004          | 7      | xF,pT      |
+| 2001  | [link][2000] | STAR       |  pi0  | AN-rap3.3        | 14     | xF,pT      |
+| 2002  | [link][2000] | STAR       |  pi0  | AN-rap3.68       | 7      | xF,pT      |
+| 2003  | [link][2000] | STAR       |  pi0  | AN-rap3.7        | 14     | xF,pT      |
+
+
+
+
+
---
+++ database/pp_AN/src/AN_pp_data.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+AN_pp_data.py - program to convert AN pp .dat data files to .xlsx
+"""
+
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#Import data as Excel files
+
+files = os.listdir('./')
+files = [f for f in files if f.endswith('.dat')]
+
+names = [f.strip('.dat') for f in files]
+
+names_dict = {"pim.2.3.200":'1000',"pim.4.200":'1001',"pip.2.3.200":'1002',"pip.4.200":'1003',
+              "piz.04":'2000',"piz.3.3":'2001',"piz.3.68":'2002',"piz.3.7":'2003'}
+
+had_dict = {"pim.2.3.200":'pi-',"pim.4.200":'pi-',"pip.2.3.200":'pi+',"pip.4.200":'pi+',
+              "piz.04":'pi0',"piz.3.3":'pi0',"piz.3.68":'pi0',"piz.3.7":'pi0'}
+
+for i in range(len(files)):
+    df = pd.read_table(files[i],sep='\s+',names=['xF','AN','stat_err','sys_err','pT'])
+    had_col = [had_dict[names[i]] for j in range(len(df.index))]
+    df['hadron'] = had_col
+    writer = pd.ExcelWriter(names_dict[names[i]]+r'.xlsx', engine='xlsxwriter')
+    df.to_excel(writer,index=False,sheet_name='Sheet1')
+    writer.save()
+        
+        
