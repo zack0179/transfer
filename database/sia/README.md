e8f5593a02a0110ab6b1f14416f2ce73d7b9393b
Alexey Prokudin <prokudin@jlab.org>
2018-04-05 16:30:24 -0400

    database fixed

--- database/sia/README.md
+++ database/sia/README.md
@@ -8,18 +8,18 @@
 | 1003  | [link][1000] | babar      | pi,pi | AUL-0-PT-INT     | 36     | z1,z2      |
 | 1004  | [link][1004] | belle      | pi,pi | AUC-0-PT-INT     | 16     | z1,z2      |
 | 1005  | [link][1004] | belle      | pi,pi | AUL-0-PT-INT     | 16     | z1,z2      |
-| 2000  | [link][2000] | babar      |  k, k | AUL-0            | 16     | z1,z2      |
-| 2001  | [link][2000] | babar      |  k, k | AUL-12           | 16     | z1,z2      |
-| 2002  | [link][2000] | babar      |  k, k | AUC-0            | 16     | z1,z2      |
-| 2003  | [link][2000] | babar      |  k, k | AUC-12           | 16     | z1,z2      |
-| 2004  | [link][2000] | babar      | pi, k | AUL-0            | 16     | z1,z2      |
-| 2005  | [link][2000] | babar      | pi, k | AUL-12           | 16     | z1,z2      |
-| 2006  | [link][2000] | babar      | pi, k | AUC-0            | 16     | z1,z2      |
-| 2007  | [link][2000] | babar      | pi, k | AUC-12           | 16     | z1,z2      |
-| 2008  | [link][2000] | babar      | pi,pi | AUL-0            | 16     | z1,z2      |
-| 2009  | [link][2000] | babar      | pi,pi | AUL-12           | 16     | z1,z2      |
-| 2010  | [link][2000] | babar      | pi,pi | AUC-0            | 16     | z1,z2      |
-| 2011  | [link][2000] | babar      | pi,pi | AUC-12           | 16     | z1,z2      |
+| 2000  | [link][2000] | babar      |  k, k | AUL-0-PT-INT           | 16     | z1,z2      |
+| 2001  | [link][2000] | babar      |  k, k | AUL-12-PT-INT           | 16     | z1,z2      |
+| 2002  | [link][2000] | babar      |  k, k | AUC-0-PT-INT           | 16     | z1,z2      |
+| 2003  | [link][2000] | babar      |  k, k | AUC-12-PT-INT           | 16     | z1,z2      |
+| 2004  | [link][2000] | babar      | pi, k | AUL-0-PT-INT            | 16     | z1,z2      |
+| 2005  | [link][2000] | babar      | pi, k | AUC-0-PT-INT           | 16     | z1,z2      |
+| 2006  | [link][2000] | babar      | pi, k | AUL-12-PT-INT           | 16     | z1,z2      |
+| 2007  | [link][2000] | babar      | pi, k | AUC-12-PT-INT           | 16     | z1,z2      |
+| 2008  | [link][2000] | babar      | pi,pi | AUL-0-PT-INT           | 16     | z1,z2      |
+| 2009  | [link][2000] | babar      | pi,pi | AUC-0-PT-INT           | 16     | z1,z2      |
+| 2010  | [link][2000] | babar      | pi,pi | AUL-12-PT-INT            | 16     | z1,z2      |
+| 2011  | [link][2000] | babar      | pi,pi | AUC-12-PT-INT           | 16     | z1,z2      |
 | 3000  | [link][3000] | bes3       | pi,pi | AUL-0            | 6      | z1,z2      |
 | 3001  | [link][3000] | bes3       | pi,pi | AUC-0            | 6      | z1,z2      |
 | 3002  | [link][3000] | bes3       | pi,pi | AUL-0            | 6      | z1,z2,pt   |
--- fitlab/inputs/sia_alexei.py
+++ fitlab/inputs/sia_alexei.py
@@ -44,33 +44,38 @@ conf['shape'] = 1
 conf['params']={}
 
 conf['params']['ff']={}
-conf['params']['ff']['widths0 pi+ fav']   = {'value':<<    1.15151579728561548333e-01>>,'fixed':False,'min':0,'max':1}
-conf['params']['ff']['widths0 pi+ unfav'] = {'value':<<    1.36784756168045196212e-01>>,'fixed':False,'min':0,'max':1}
-conf['params']['ff']['widths0 k+ fav']    = {'value':<<    1.34062655687440246410e-01>>,'fixed':False,'min':0,'max':1}
-conf['params']['ff']['widths0 k+ unfav']  = {'value':<<    1.87915286213150012351e-01>>,'fixed':False,'min':0,'max':1} 
+conf['params']['ff']['widths0 pi+ fav']   = {'value':1.15151579728561548333e-01,'fixed':True,'min':0,'max':1}
+conf['params']['ff']['widths0 pi+ unfav'] = {'value':1.36784756168045196212e-01,'fixed':True,'min':0,'max':1}
+conf['params']['ff']['widths0 k+ fav']    = {'value':<<    1.34062655687440246410e-01>>,'fixed':True,'min':0,'max':1} 
 
 conf['params']['collins']={}
-conf['params']['collins']['widths0 pi+ fav']     = {'value':<<    7.47958632885192820083e-02>>,'fixed':False,'min':1e-5,'max':1}
-conf['params']['collins']['widths0 pi+ unfav']   = {'value':<<    1.59763576917398841815e+00>>,'fixed':False,'min':1e-5,'max':2}
+conf['params']['collins']={}
+conf['params']['collins']['widths0 pi+ fav']     = {'value':0.07502,'fixed':False,'min':0.05,'max':0.1}
+conf['params']['collins']['widths0 pi+ unfav']   = {'value':0.07649,'fixed':'widths0 pi+ fav','min':1e-5,'max':0.2}
+conf['params']['collins']['pi+ u N 1']  = {'value': 1.01381,'fixed':False,'min':0,'max':4}
+conf['params']['collins']['pi+ u a 1']  = {'value':-1.58758,'fixed':False,'min':-0.5,'max':0}
+conf['params']['collins']['pi+ u b 1']  = {'value': 4.77410,'fixed':False,'min':3.,'max':7.}
 
-conf['params']['collins']['pi+ u N 1']  = {'value':<<    4.49239476315589936206e+00>> ,'fixed':False,'min':-10,'max':10}
-conf['params']['collins']['pi+ u a 1']  = {'value':<<   -8.28098465048009213518e-01>> ,'fixed':False,'min':-1,'max':5}
-conf['params']['collins']['pi+ u b 1']  = {'value':<<    6.60780815284983358282e+00>> ,'fixed':False,'min':1e-5,'max':10}
-conf['params']['collins']['pi+ u c 1']  = {'value':<<    0.00000000000000000000e+00>> ,'fixed':True,'min':-10,'max':10}
-conf['params']['collins']['pi+ u d 1']  = {'value':<<    0.00000000000000000000e+00>> ,'fixed':True,'min':-10,'max':10}
+conf['params']['collins']['pi+ d N 1']  = {'value':-7.94373,'fixed':False,'min':-15,'max':-4}
+conf['params']['collins']['pi+ d a 1']  = {'value': 3.14003,'fixed':False,'min': 0.,'max':4.5}
+conf['params']['collins']['pi+ d b 1']  = {'value': 3.27627,'fixed':False,'min':2.5,'max':3.8}
 
-conf['params']['collins']['pi+ u N 2']  = {'value':<<    6.11150078306284516572e+00>> ,'fixed':False,'min':-10,'max':10}
-conf['params']['collins']['pi+ u a 2']  = {'value':<<    2.71508421683099232524e+00>> ,'fixed':False,'min':-1,'max':5}
-conf['params']['collins']['pi+ u b 2']  = {'value':<<    2.75387728005980081392e+00>> ,'fixed':False,'min':1e-5,'max':10}
-conf['params']['collins']['pi+ u c 2']  = {'value':<<    0.00000000000000000000e+00>> ,'fixed':True,'min':-10,'max':10}
-conf['params']['collins']['pi+ u d 2']  = {'value':<<    0.00000000000000000000e+00>> ,'fixed':True,'min':-10,'max':10}
+conf['params']['collins']['pi+ u c 1']  = {'value': 0.0,'fixed':True,'min':-10,'max':10}
+conf['params']['collins']['pi+ d c 1']  = {'value': 0.0,'fixed':True,'min':-10,'max':10}
+conf['params']['collins']['pi+ u d 1']  = {'value': 0.0,'fixed':True,'min':-10,'max':10}
+conf['params']['collins']['pi+ d d 1']  = {'value': 0.0,'fixed':True,'min':-10,'max':10}
 
-conf['params']['collins']['pi+ d N 1']  = {'value':<<   -4.14852904854067539020e+00>> ,'fixed':False,'min':-10,'max':10}
-conf['params']['collins']['pi+ d a 1']  = {'value':<<   -1.00000000000000000000e+00>> ,'fixed':False,'min':-3,'max':5}
-conf['params']['collins']['pi+ d b 1']  = {'value':<<    6.37348461151638101541e+00>> ,'fixed':False,'min':1e-5,'max':10}
-conf['params']['collins']['pi+ d c 1']  = {'value':<<    0.00000000000000000000e+00>> ,'fixed':True,'min':-10,'max':10}
-conf['params']['collins']['pi+ d d 1']  = {'value':<<    0.00000000000000000000e+00>> ,'fixed':True,'min':-10,'max':10}
+conf['params']['collins']['pi+ u N 2']  = {'value': 6.31940,'fixed':False,'min':1,'max':12}
+conf['params']['collins']['pi+ u a 2']  = {'value': 5.53240,'fixed':False,'min':1,'max':10}
+conf['params']['collins']['pi+ u b 2']  = {'value': 3.32471,'fixed':False,'min':3,'max':5}
 
+conf['params']['collins']['pi+ d N 2']  = {'value': 0.0,'fixed':True,'min':-20,'max':0}
+conf['params']['collins']['pi+ d a 2']  = {'value': 0.0,'fixed':True,'min':-1,'max':5}
+conf['params']['collins']['pi+ d b 2']  = {'value': 0.0,'fixed':True,'min':1e-5,'max':10}
+conf['params']['collins']['pi+ u c 2']  = {'value': 0.0,'fixed':True,'min':-10,'max':10}
+conf['params']['collins']['pi+ d c 2']  = {'value': 0.0,'fixed':True,'min':-10,'max':10}
+conf['params']['collins']['pi+ u d 2']  = {'value': 0.0,'fixed':True,'min':-10,'max':10}
+conf['params']['collins']['pi+ d d 2']  = {'value': 0.0,'fixed':True,'min':-10,'max':10}
 
 ############################################################################
 # set data sets
@@ -98,6 +103,8 @@ conf['datasets']['sia']['xlsx'][1002]='../database/sia/expdata/1002.xlsx' # baba
 conf['datasets']['sia']['xlsx'][1003]='../database/sia/expdata/1003.xlsx' # babar | pi,pi | AUL-0     | 36     | z1,z2      |    
 conf['datasets']['sia']['xlsx'][1004]='../database/sia/expdata/1004.xlsx' # belle | pi,pi | AUT-0-CCP | 16     | z1,z2,qT   |    
 conf['datasets']['sia']['xlsx'][1005]='../database/sia/expdata/1005.xlsx' # belle | pi,pi | AUT-0     | 16     | z1,z2,qT   |   
+conf['datasets']['sia']['xlsx'][2008]='../database/sia/expdata/2008.xlsx' # | babar      | pi,pi | AUL-0            | 16     | z1,z2      |    
+conf['datasets']['sia']['xlsx'][2009]='../database/sia/expdata/2009.xlsx' # | babar      | pi,pi | AUC-0            | 16     | z1,z2      |   
 
 for k in conf['datasets']['sia']['xlsx']: conf['datasets']['sia']['norm'][k]={'value':1,'fixed':True,'min':0,'max':1} 
 
